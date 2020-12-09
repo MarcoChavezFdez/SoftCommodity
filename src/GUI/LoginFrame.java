4a8f60d233618470e8b7b05ee7fac5599225ade9
CarlosdeAnda <jesparzaz301@accitesz.com>
2020-12-09 01:07:33 -0600

    Error login - Ventana Emergente

--- src/GUI/LoginFrame.java
+++ src/GUI/LoginFrame.java
@@ -19,6 +19,7 @@ public class LoginFrame extends javax.swing.JFrame {
      */
     public LoginFrame() {
         initComponents();
+        jOptionPane1.setVisible(false);
     }
 
     /**
@@ -32,6 +33,8 @@ public class LoginFrame extends javax.swing.JFrame {
 
         jDialog1 = new javax.swing.JDialog();
         lbl_Error = new javax.swing.JLabel();
+        jPopupMenu1 = new javax.swing.JPopupMenu();
+        jFrame1 = new javax.swing.JFrame();
         lblFondo = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
@@ -41,6 +44,7 @@ public class LoginFrame extends javax.swing.JFrame {
         jPass_Usuario = new javax.swing.JPasswordField();
         jl_Mensajes = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
+        jOptionPane1 = new javax.swing.JOptionPane();
 
         jDialog1.getContentPane().setLayout(new java.awt.GridLayout(3, 0, 0, 3));
 
@@ -105,10 +109,21 @@ public class LoginFrame extends javax.swing.JFrame {
         jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Img/LOGO_SoftCommodity+_V1.png"))); // NOI18N
         jPanel2.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 40, 290, 130));
 
+        jOptionPane1.setBackground(new java.awt.Color(237, 174, 195));
+        jOptionPane1.setForeground(new java.awt.Color(237, 174, 195));
+        jOptionPane1.setMessage("El usuario o contraseña no existen");
+        jOptionPane1.setEnabled(false);
+        jOptionPane1.setFocusable(false);
+        jPanel2.add(jOptionPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 270, -1, -1));
+
         getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 510, 450));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jPass_UsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jPass_UsuarioActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jPass_UsuarioActionPerformed
 
     private void txtF_UsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtF_UsuarioActionPerformed
 
@@ -123,7 +138,7 @@ public class LoginFrame extends javax.swing.JFrame {
             Usuario currentUser = conexion.consultarUsuarioLogin(login);
 
             if (currentUser.getIdUsuario() == null) {
-                jl_Mensajes.setText("El usuario que ingreso no existe");
+                jOptionPane1.showMessageDialog(rootPane, "El usuario que ingreso no existe");
             } else if (currentUser.getPassw().equals(pass)) {
                 MenuPrincipalFrame menu = new MenuPrincipalFrame(conexion);
                 menu.setVisible(true);
@@ -131,20 +146,14 @@ public class LoginFrame extends javax.swing.JFrame {
                 this.setVisible(false);
 
             } else {
-                jl_Mensajes.setText("Usuario y/o contraseñas incorrecctos");
+                jOptionPane1.showMessageDialog(rootPane, "Usuario y/o contraseñas incorrecctos");
             }
 
         } catch (Exception e) {
             System.out.println("Error");
             lbl_Error.setText(e.getMessage());
         }
-
-
     }//GEN-LAST:event_jButton1ActionPerformed
-
-    private void jPass_UsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jPass_UsuarioActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jPass_UsuarioActionPerformed
 
     /**
      * @param args the command line arguments
@@ -185,11 +194,14 @@ public class LoginFrame extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JDialog jDialog1;
+    private javax.swing.JFrame jFrame1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JOptionPane jOptionPane1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPasswordField jPass_Usuario;
+    private javax.swing.JPopupMenu jPopupMenu1;
     private javax.swing.JLabel jl_Mensajes;
     private javax.swing.JLabel lblFondo;
     private javax.swing.JLabel lbl_Error;
